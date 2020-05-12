9d059c0bdd84d430668bcbbc6a4b024e8990f00f
kcyun1410 <kcyun1410@gmail.com>
2020-05-12 16:30:26 +0900

    2020_05_12 3강 야구게임완료

---
+++ 3강/main.js
@@ -0,0 +1,9 @@
+//vue instance
+//설치한 vue를 가져옴.
+import Vue from 'vue';
+import NumberBaseball from './NumberBaseball';
+
+//el역활을 함(vue가 통제할 div 태그id)
+new Vue(NumberBaseball).$mount('#root');
+
+//main.js가 수정 또는 추가될 때마다 다시 빌드해줘야 함
