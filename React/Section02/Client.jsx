f6f0097e0878ff7f783495e4bd8987af22fdb966
kcyun1410 <kcyun1410@gmail.com>
2020-05-19 17:42:06 +0900

    2020-05-19 보고

---
+++ React/Section02/Client.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const { hot } = require('react-hot-loader/root');
+
+
+ const WordRelay = require('./WordRelay');
+//const RenderTest = require('./RenderTest');
+
+// const Hot = hot(RenderTest);
+
+// ReactDom.render(<Hot />, document.querySelector("#root"));
+
+const Hot = hot(WordRelay);
+
+ReactDom.render(<WordRelay />, document.querySelector("#root"));
