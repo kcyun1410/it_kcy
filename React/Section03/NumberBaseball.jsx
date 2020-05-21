f87bd08c41752487701e946db3898d11dd29b8d7
kcyun1410 <kcyun1410@gmail.com>
2020-05-21 17:32:04 +0900

    2020_05_21 보고

--- React/Section03/NumberBaseball.jsx
+++ React/Section03/NumberBaseball.jsx
@@ -1,19 +1,92 @@
 const React = require('react');
-const { Component } = React;
+import Try from './Try';
 
-class Try extends React.Component {
+function getNumbers() {
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9-i)), 1)[0];
+    }
+}
+class NumberBaseball extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            answer: '',
+            value: '',
+            result: getNumbers(), // ex. [1,3,5,7]
+            tries: [],
+        }
+        this.onChangeInput = this.onChangeInput.bind(this);
+    }
+
+    fruits = [
+        {fruit : '사과', taste : '맛있다'}, 
+        {fruit:'바나나', taste:'달다'}, 
+        {fruit:'귤', taste:'달아'}
+    ];
+
+    onSubmitForm = (e) => {
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런',
+                tries: [...this.state.tries, {
+                    try: this.state.value, result: '홈런'
+                }],
+
+            });
+
+        } else {
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) {
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
+                });
+                alert('게임을 다시 시작합니다!');
+            } else {
+                for (let i = 0; i < 4; i += 1) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes([i])) {
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, {
+                        try: this.state.value,
+                        result: `${strike} 스트라이크, ${ball} 볼입니다`
+                    }]
+                })
+            }
+        }
+    }
+
+    onChangeInput = (e) => {
+
+    }
+
+
     render() {
-        // const { v } = this.props;
-        const { value, index } = this.props;
-
         return (
-            <React.Fragment>
-                <li key={value.fruit + value.taste}>{value.fruit}-{value.taste}</li>
-                <p>c1</p>
-                <p>c1</p>
-            </React.Fragment>
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}></input>
+                    <div>
+                        시도: {this.state.tries.length}
+                    </div>
+                    <ol>
+                        {this.fruits.map((v, i) => {
+                            return (
+                                <Try value={v} index={i}/>
+                            );
+                        })}
+                    </ol>
+                </form>
+            </>
         );
     }
 }
 
-export default Try;
+module.exports = NumberBaseball;
---
+++ React/Section04/ResponseCheck.jsx
@@ -0,0 +1,69 @@
+const React = require('react');
+
+class ResponseCheck2 extends React.Component {
+    state = {
+        state: 'waiting',
+        message: '클릭해서 시작하세요',
+        result: [3, 4, 5],
+    };
+
+    timeout;
+
+    onClickScreen = () => {
+        const { state, message, result } = this.state;
+        console.log(state);
+
+        if (state === 'waiting') {
+            this.setState({
+                state: 'ready',
+                message: '초록색이 되면 클릭하세요',
+            });
+            this.timeout = setTimeout(() => {
+                this.setState({
+                    state: 'now',
+                    message: '지금 클릭',
+                })
+            }, Math.floor(Math.random() * 1000) + 2000);
+
+        } else if (state === 'ready') { // 성급하게 클릭
+            clearTimeout(this.timeout);
+            this.setState({
+                state:'waiting',
+                message: '이런 성급하셨군요',
+            });
+            
+        } else if (state === 'now') { // 반응속도 체크
+            this.setState({
+                state: 'waiting',
+            });
+        }
+    }
+
+    renderAverage = () => {
+        
+    }
+
+    render() {
+        return (
+            <>
+                <div id="screen" 
+                    className={this.state.state}
+                    onClick={this.onCLickScreen}
+                >
+                    {this.state.message}
+                </div>
+                
+                {
+                    this.state.result.length === 0 
+                    ? null 
+                    : <div>
+                        평균 시간: {
+                            this.state.result.reduce((a, c) => a + c) / this.state.result.length
+                        }ms
+                    </div>
+                }
+            </>
+        );
+    }
+}
+module.exports = ResponseCheck2;
